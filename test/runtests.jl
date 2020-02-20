2597b71831fc5546d2a8c7434d3dee10aeeb1940
Richard Dennis <richard.dennis@glasgow.ac.uk>
2020-02-20 23:45:36 +0000

    Moved old tests to "old" folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,7 @@
-include("linear_example.jl")
-include("second_order_example.jl")
+using Pkg
+Pkg.add("https://github.com/RJDennis/ChebyshevApprox.jl")
+Pkg.add("https://github.com/RJDennis/SmolyakApprox.jl")
+Pkg.add("https://github.com/RJDennis/PiecewiseLinearApprox.jl")
+
+include("old/linear_example.jl")
+include("old/second_order_example.jl")
