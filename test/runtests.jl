4d7abed668e77240976c8594dcf892878d9534ab
Richard Dennis <richard.dennis@glasgow.ac.uk>
2020-02-21 09:48:51 +0000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,2 @@
-using Pkg
-Pkg.add("https://github.com/RJDennis/ChebyshevApprox.jl")
-Pkg.add("https://github.com/RJDennis/SmolyakApprox.jl")
-Pkg.add("https://github.com/RJDennis/PiecewiseLinearApprox.jl")
-
 include("old/linear_example.jl")
 include("old/second_order_example.jl")
