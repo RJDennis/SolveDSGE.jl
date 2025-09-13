d9c02c764c1fcec15fc2ab8a2b60b69b26be8330
Richard Dennis <richard.dennis@glasgow.ac.uk>
2025-09-13 02:25:18 +0100

    ??

--- src/SolveDSGE.jl
+++ src/SolveDSGE.jl
@@ -10,7 +10,7 @@ using SmolyakApprox
 using PiecewiseLinearApprox
 using HyperbolicCrossApprox
 using NLboxsolve
-import OrderedCollections
+import OrderedCollections: OrderedDict
 using Distributions
 
 include("structures.jl")
