76b4744db70f80b4fa39a0f82bb5bae69eb2d5c1
Richard Dennis <richard.dennis@glasgow.ac.uk>
2025-09-16 18:37:04 +0100

    Testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
 include("solution_file_1a.jl")
-include("solution_file_2a.jl")
-include("solution_file_1b.jl")
-include("solution_file_2b.jl")
+#include("solution_file_2a.jl")
+#include("solution_file_1b.jl")
+#include("solution_file_2b.jl")
